c2f174460639bede9c352265000ca117e3eb10d0
Алена Егупова <a.egupova@free-lance.ru>
2022-02-22 10:54:32 +0300

    Fixed wallService and added tests

--- src/main/kotlin/ru/netology/service/WallService.kt
+++ src/main/kotlin/ru/netology/service/WallService.kt
@@ -16,31 +16,9 @@ class WallService {
     fun update(post: Post): Boolean {
         for((index, existPost) in posts.withIndex()){
             if (existPost.id == post.id){
-                posts[index] = post.copy(
+                posts[index] = existPost.copy(
                     id = existPost.id,
-                    ownerId = existPost.ownerId,
-                    fromId = post.fromId,
-                    createdBy = post.createdBy,
-                    date = post.date,
-                    text = post.text,
-                    replyOwnerId = post.replyOwnerId,
-                    replyPostId = post.replyPostId,
-                    friendsOnly = post.friendsOnly,
-                    comments = post.comments,
-                    copyright = post.copyright,
-                    likes = post.likes,
-                    reposts = post.reposts,
-                    views = post.views,
-                    postType = post.postType,
-                    signerId = post.signerId,
-                    canPin = post.canPin,
-                    canDelete = post.canDelete,
-                    canEdit = post.canEdit,
-                    isPinned = post.isPinned,
-                    markedAsAds = post.markedAsAds,
-                    isFavourite = post.isFavourite,
-                    donut = post.donut,
-                    postponedId = post.postponedId
+                    ownerId = existPost.ownerId
                 )
                 return true
             }
--- src/test/kotlin/ru/netology/service/WallServiceTest.kt
+++ src/test/kotlin/ru/netology/service/WallServiceTest.kt
@@ -1,7 +1,6 @@
 package ru.netology.service
 
-import junit.framework.Assert.assertTrue
-import junit.framework.AssertionFailedError
+import junit.framework.Assert.*
 import org.junit.Test
 import ru.netology.data.Post
 
@@ -40,7 +39,7 @@ class WallServiceTest {
 
         service.add(Post(
             id = 0,
-            ownerId = 23,
+            ownerId = 56,
             fromId = 65,
             createdBy = 3,
             date = 25341,
@@ -92,5 +91,100 @@ class WallServiceTest {
 
         val result = service.update(update)
         assertTrue(result)
+    }
+
+    @Test
+    fun updateNotExisting() {
+        val service = WallService()
+        service.add(Post(
+            id = 0,
+            ownerId = 1,
+            fromId = 1,
+            createdBy = 1,
+            date = 12345,
+            text = "First post",
+            replyOwnerId = 3,
+            replyPostId = 2,
+            friendsOnly = true,
+            comments = {},
+            copyright = {},
+            likes = {},
+            reposts = {},
+            views = {},
+            postType = "Type first",
+            canDelete = true,
+            canEdit = true,
+            canPin = true,
+            donut = {},
+            isFavourite = false,
+            isPinned = 1,
+            markedAsAds = false,
+            postponedId = 3,
+            signerId = 2
+        ))
+
+        val update = Post(
+            id = 0,
+            ownerId = 23,
+            fromId = 65,
+            createdBy = 3,
+            date = 25341,
+            text = "Updated post",
+            replyOwnerId = 34,
+            replyPostId = 21,
+            friendsOnly = false,
+            comments = {},
+            copyright = {},
+            likes = {},
+            reposts = {},
+            views = {},
+            postType = "Type updated",
+            canDelete = false,
+            canEdit = false,
+            canPin = false,
+            donut = {},
+            isFavourite = false,
+            isPinned = 0,
+            markedAsAds = true,
+            postponedId = 23,
+            signerId = 25)
+
+        val result = service.update(update)
+        assertFalse(result)
+    }
+
+    @Test
+    fun addPost() {
+        val service = WallService()
+        val post = Post(
+            id = 0,
+            ownerId = 1,
+            fromId = 1,
+            createdBy = 1,
+            date = 12345,
+            text = "First post",
+            replyOwnerId = 3,
+            replyPostId = 2,
+            friendsOnly = true,
+            comments = {},
+            copyright = {},
+            likes = {},
+            reposts = {},
+            views = {},
+            postType = "Type first",
+            canDelete = true,
+            canEdit = true,
+            canPin = true,
+            donut = {},
+            isFavourite = false,
+            isPinned = 1,
+            markedAsAds = false,
+            postponedId = 3,
+            signerId = 2
+        )
+
+        val result = service.add(post)
+        assertEquals(post.copy(id = result.id), result)
+
     }
 }
